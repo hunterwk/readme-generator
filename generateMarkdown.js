c6ed15b61191df7bbb8ba4de4bb573cc63d93e17
Hunter Kantner <hunterkantner@gmail.com>
2020-10-01 17:31:15 -0700

    created folder to store generated readme, and added gitignore

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,41 +1,121 @@
 //function to generate the markdown file
 const fs = require("fs");
 const util = require("util");
-const inquirer = require("inquirer")
-//const {Console} = require("console";)
+const inquirer = require("inquirer");
+const {Console} = require("console");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+inquirer
+    .prompt ([
+        {
+            type: "input",
+            message: "What is your title?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "Enter your description:",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "Enter installation instructions:",
+            name: "install",
+        },
+        {
+            type: "input",
+            message: "How can you use this app?",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "Define contribution guidelines:",
+            name: "contribution",
+        },
+        {
+            type: "input",
+            message: "Define testing instructions:",
+            name: "tests" ,
+        },
+        {
+            type: "input",
+            message: "Which license would you like to use?",
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "Enter your github username:",
+            name: "github",
+        },
+        {
+            type: "input",
+            message: "Enter your email address:",
+            name: "email",
+        }
 
 
-const data = {
-    title: 'Hello World!',
-    description: "it's a description",
-    installation: "how to install",
-    usage: "what to use this for",
-    contributing: "contribution guidelines",
-    tests: "test instructions",
-    license: "MIT probably",
-    github: "github username",
-    email: "email",
+    ])
+    .then((response) => {
+        const {title} = response;
+        const {description} = response;
+        const {install} = response;
+        const {usage} = response;
+        const {contribution} = response;
+        const {tests} = response;
+        const {license} = response;
+        const {github} = response;
+        const {email} = response;
 
-}
 
-function generateMarkdown(data) {
-    return `# ${data.title}
-    (LICENSE BADGE HERE)
-    ## Description
-    ${data.description}
+        let generateReadme = 
+        `# ${response.title}
+        (LICENSE BADGE HERE)
+        ## Description
+        ${response.description}
+        
+        ## Installation Instructions
+        ${response.installation}
+        
+        ## Usage Instructions
+        ${response.usage}
     
-    ## Installation Instructions
-    ${data.installation}
-    
-    ## Usage Instructions
-    ${data.usage}
+        ## Contribution Guidelines
+        ${response.contribution}
+        
+        ## Questions?
+        ${response.github}
+        or email me at ${response.email}
+        
+        `
 
-    ## Contribution Guidelines
-    ${data.contributing}
+        writeFileAsync('./Generated/README.MD', generateReadme);
+    })
+    .then(() => console.log("file made successfully!"));
+    // .catch((err) => {
+    //     console.log(err);
+    // });
+
+
+// function generateMarkdown(response) {
+//     return `# ${response.title}
+//     (LICENSE BADGE HERE)
+//     ## Description
+//     ${response.description}
     
-    ## Questions?
-    ${data.github}
-    or email me at ${data.email}
+//     ## Installation Instructions
+//     ${response.installation}
     
-    `
-}
+//     ## Usage Instructions
+//     ${response.usage}
+
+//     ## Contribution Guidelines
+//     ${response.contributing}
+    
+//     ## Questions?
+//     ${response.github}
+//     or email me at ${response.email}
+    
+//     `
+// }
+// console.log(response)
